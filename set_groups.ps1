85f81230fb1a29d2709c0d40caada2c27cc5e80d
Bert Proesmans <bproesmans@hotmail.com>
2024-08-14 23:46:38 +0200

    Updated set_groups.ps1
    Automatically resolve local computer name

--- set_groups.ps1
+++ set_groups.ps1
@@ -1,10 +1,17 @@
 #Requires -RunAsAdministrator
 
-$User = "B-PC\Bert"
-$Targets = @(
-    "Network Configuration Operators"
-    "Hyper-V Administrators"
+$User = "Bert" # CHANGEME; Your username
+$Targets = @( # CHANGEME; The admin groups you want to be a part of (see windows documentation)
+    # REF; https://learn.microsoft.com/en-us/windows-server/identity/ad-ds/manage/understand-security-groups#default-active-directory-security-groups
+    
+    "Network Configuration Operators" # Allows the user to reconfigure network settings on network adapters (but not enable/disable them)
+    
+    "Hyper-V Administrators" # Allows the user to manage virtual machines and virtual networks, also manage virtual machine state
 )
 
+# The username must be resolved at the local computer scope. AKA to setup the user in an ACL rule we have to transform
+# the username reference to "<computername>\<username>"" eg mycomputer\myuser
+$ComputerName = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+
 $Targets | `
-    Foreach-Object { & net localgroup "$_" "$User" /add }
+    Foreach-Object { & net localgroup "$_" "$ComputerName\$User" /add }
