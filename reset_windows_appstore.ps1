fe5d28124c30ad9ad5c1d7c1a7a97d476dc22c63
Bert Proesmans <bproesmans@hotmail.com>
2024-11-30 00:28:11 +0100

    Cleanup code structure and comments of powershell scripts

---
+++ reset_windows_appstore.ps1
@@ -0,0 +1,7 @@
+#Requires -RunAsAdministrator
+
+# This resets the windows store + store cache.
+# Wait for this process to complete asynchronously
+#
+# WARN; This command requires internet connection
+wsreset -i
