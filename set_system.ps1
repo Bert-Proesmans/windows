767591ab080a53b5ef05e232e39e43a2811b4c7b
Bert Proesmans <bproesmans@hotmail.com>
2024-11-30 21:07:49 +0100

    Add commands to manage ssh services

--- set_system.ps1
+++ set_system.ps1
@@ -2,3 +2,6 @@
 
 # Disables hibernation, removing hiberfil.sys, freeing ~50% of RAM in disk space
 POWERCFG /HIBERNATE OFF
+
+Get-Service sshd | Set-Service -StartupType Disabled
+Get-Service ssh-agent | Set-Service -StartupType Automatic
