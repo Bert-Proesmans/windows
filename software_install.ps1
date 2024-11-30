77f05784c70d231b5c1553e71af660c23171bd0d
Bert Proesmans <bproesmans@hotmail.com>
2024-11-30 01:14:38 +0100

    Update winget and software install scripts for correctness

--- software_install.ps1
+++ software_install.ps1
@@ -10,6 +10,30 @@ if (!(Get-Command "winget" -ErrorAction SilentlyContinue)) {
 # TODO - Include package and if it should be updated into a single table
 # TODO - Autogenerate the app update whitelist based on the table
 
+# Configure WAU and install
+$registryPath = "HKLM:\Software\Policies\Romanitho\Winget-AutoUpdate"
+if (!(Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+
+# Winget AutoUpdater GPO keys
+$data = @{
+    "WAU_ActivateGPOManagement" = 1
+    "WAU_DisableAutoUpdate" = 0
+    "WAU_UseWhiteList" = 1
+    # MUST BE a folder path!
+    "WAU_ListPath" = $programPath
+    "WAU_UpdatesInterval" = "Daily"
+    "WAU_StartMenuShortcut" = 1
+    "WAU_DesktopShortcut" = 0
+}
+$data.Keys | ForEach-Object {
+    Set-ItemProperty -Path $registryPath -Name $_ -Value $data[$_]
+}
+
+# TODO - Update WAU install. They fixed the whitelist overwrite
+
+
 @(
     "7zip.7zip"
     "Almico.SpeedFan"
@@ -38,6 +62,29 @@ if (!(Get-Command "winget" -ErrorAction SilentlyContinue)) {
     "Yubico.YubikeyManager"
 ) | ForEach-Object { winget.exe install --accept-source-agreements --accept-package-agreements --source winget --exact --id $_ }
 
+# Git needs to be installed manually because it doesn't want to elevate itself.
+# Git.Git --override '/DIR="C:\Program Files\Git" /ALLUSERS /COMPONENTS="gitlfs,scalar"'
+
+# Custom install for VSCode
+winget.exe install --source winget --scope machine --exact --id Microsoft.VisualStudioCode `
+    --override '/verysilent /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"'
+
+# Install rust pre-requisites
+winget.exe install --source winget --exact --id Microsoft.VisualStudio.2022.Community `
+    --override "--add Microsoft.VisualStudio.Workload.NativeDesktop;includeRecommended --focusedUi --wait"
+
+#
+$processUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$sessionUser = (Get-WmiObject -Query "SELECT UserName FROM Win32_ComputerSystem").UserName
+if ($processUser -ne $sessionUser) 
+{
+    Write-Host ("**WARNING*; The user owning the current process is _NOT_ the logon session user." +
+    " This discrepancy will cause confusion as the next software will be installed in USER CONTEXT, which is the context of the process user!" +
+    " The process user is relevant for both user-installs of software and windows store applications.")
+    Write-Host "Press enter to continue..."
+    Read-Host
+}
+
 # This software MUST be executed as the user that is going to run it.
 # Using a separate administrator user to install it will not make the software available to all users!
 @(
@@ -51,62 +98,8 @@ if (!(Get-Command "winget" -ErrorAction SilentlyContinue)) {
     "UderzoSoftware.SpaceSniffer"
 ) | ForEach-Object { winget.exe install --accept-source-agreements --accept-package-agreements --source winget --exact --id $_ }
 
-# Git needs to be installed manually because it doesn't want to elevate itself.
-# Git.Git --override '/DIR="C:\Program Files\Git" /ALLUSERS /COMPONENTS="gitlfs,scalar"'
-
-# Custom install for VSCode
-winget.exe install --source winget --scope machine --exact --id Microsoft.VisualStudioCode `
-    --override '/verysilent /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"'
-
 @(
     "9NBLGGH516XP" # Eartrumpet
     "9P7KNL5RWT25" # Sysinternal (zoomit)
     "9PLJWWSV01LK" # TwinkleTray
 ) | ForEach-Object { winget.exe install --accept-source-agreements --accept-package-agreements --source msstore --exact --id $_ }
-
-# Install rust pre-requisites
-winget.exe install --source winget --exact --id Microsoft.VisualStudio.2022.Community `
-    --override "--add Microsoft.VisualStudio.Workload.NativeDesktop;includeRecommended --focusedUi --wait"
-
-# TODO - Update WAU install. They fixed the whitelist overwrite
-
-# Configure winget auto-updater
-$programPath = [Environment]::GetFolderPath("COMMONAPPLICATIONDATA") + "\Winget-AutoUpdate-data"
-if (!(Test-Path $programPath)) {
-    New-Item -Path $programPath -ItemType Directory -Force | Out-Null
-}
-# Cannot store file within WingetAutoUpdate programdata folder, the folder contents are overwritten during install
-$programWhitelist = $programPath + "\included_apps.txt"
-Copy-Item -Path .\wau_whitelist.txt -Destination $programWhitelist
-
-$registryPath = "HKLM:\Software\Policies\Romanitho\Winget-AutoUpdate"
-if (!(Test-Path $registryPath)) {
-    New-Item -Path $registryPath -Force | Out-Null
-}
-
-# Winget AutoUpdater GPO keys
-$data = @{
-    "WAU_ActivateGPOManagement" = 1
-    "WAU_DisableAutoUpdate" = 0
-    "WAU_UseWhiteList" = 1
-    # MUST BE a folder path!
-    "WAU_ListPath" = $programPath
-    "WAU_UpdatesInterval" = "Daily"
-    "WAU_StartMenuShortcut" = 1
-    "WAU_DesktopShortcut" = 0
-}
-$data.Keys | ForEach-Object {
-    Set-ItemProperty -Path $registryPath -Name $_ -Value $data[$_]
-}
-
-# Install Winget Auto Update
-$url = Invoke-RestMethod -Uri "https://api.github.com/repos/Romanitho/Winget-AutoUpdate/releases/latest" | `
-    Select-Object -ExpandProperty assets | `
-    Where-Object {"WAU.zip" -eq $_.name} | `
-    Select-Object -ExpandProperty browser_download_url
-
-$downloadPath = $env:TEMP + "\WAU.zip"
-Invoke-WebRequest -Uri $url -OutFile $downloadPath -Force
-$unzippedPath = $env:TEMP + "\WAU"
-Expand-Archive -Path $downloadPath -DestinationPath $unzippedPath -Force
-Start-Process -Wait -FilePath ($unzippedPath + "\install.bat")
--- winget_install.ps1
+++ winget_install.ps1
@@ -4,26 +4,127 @@
 #
 # If there is no store (IoT release) or it's broken, run reset_windows_appstore.ps1 first!
 
-# == First ensure winget is installed and updated to the latest version ==
+<#
+.SYNOPSIS
+    Synchronously triggers store updates for a select set of apps. You should run this in
+    legacy powershell.exe, as some of the code has problems in pwsh on older OS releases.
 
+    REF; https://github.com/microsoft/winget-cli/discussions/1738#discussioncomment-5484927
+#>
+try
+{
+    if ($PSVersionTable.PSVersion.Major -ne 5)
+    {
+        throw "This script has problems in pwsh on some platforms; please run it with legacy Windows PowerShell (5.1) (powershell.exe)."
+    }
+
+    $processUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $sessionUser = (Get-WmiObject -Query "SELECT UserName FROM Win32_ComputerSystem").UserName
+
+    if ($processUser -ne $sessionUser)
+    {
+        throw "Due to WinRT limitations, this script must run as the same user that created this logon session; please make the current user administrator and run this script again."
+    }
+
+    # https://fleexlab.blogspot.com/2018/02/using-winrts-iasyncoperation-in.html
+    Add-Type -AssemblyName System.Runtime.WindowsRuntime
+    $asTaskGeneric = ([System.WindowsRuntimeSystemExtensions].GetMethods() | ? { $_.Name -eq 'AsTask' -and $_.GetParameters().Count -eq 1 -and $_.GetParameters()[0].ParameterType.Name -eq 'IAsyncOperation`1' })[0]
+    function Await($WinRtTask, $ResultType) {
+        $asTask = $asTaskGeneric.MakeGenericMethod($ResultType)
+        $netTask = $asTask.Invoke($null, @($WinRtTask))
+        $netTask.Wait(-1) | Out-Null
+        $netTask.Result
+    }
+
+    # https://docs.microsoft.com/uwp/api/windows.applicationmodel.store.preview.installcontrol.appinstallmanager?view=winrt-22000
+    # We need to tell PowerShell about this WinRT API before we can call it...
+    Write-Host "Enabling Windows.ApplicationModel.Store.Preview.InstallControl.AppInstallManager WinRT type"
+    [Windows.ApplicationModel.Store.Preview.InstallControl.AppInstallManager,Windows.ApplicationModel.Store.Preview,ContentType=WindowsRuntime] | Out-Null
+    $appManager = New-Object -TypeName Windows.ApplicationModel.Store.Preview.InstallControl.AppInstallManager
+
+    # Customize this list of apps to suit... the key one for making sure that winget.exe is
+    # installed is the DesktopAppInstaller one.
+    $appsToUpdate = @(
+        "Microsoft.WindowsStore_8wekyb3d8bbwe",
+        "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe"  # <-- winget comes from this one
+    )
+
+    foreach ($app in $appsToUpdate)
+    {
+        try
+        {
+            Write-Host "Requesting an update for $app..."
+            $updateOp = $appManager.UpdateAppByPackageFamilyNameAsync($app)
+            $updateResult = Await $updateOp ([Windows.ApplicationModel.Store.Preview.InstallControl.AppInstallItem])
+            while ($true)
+            {
+                if ($null -eq $updateResult)
+                {
+                    Write-Host "Update is null. It must already be completed (or there was no update)..."
+                    break
+                }
+
+                if ($null -eq $updateResult.GetCurrentStatus())
+                {
+                    Write-Host "Current status is null. WAT"
+                    break
+                }
+
+                Write-Host $updateResult.GetCurrentStatus().PercentComplete
+                if ($updateResult.GetCurrentStatus().PercentComplete -eq 100)
+                {
+                    Write-Host "Install completed ($app)"
+                    break
+                }
+                Start-Sleep -Seconds 3
+            }
+        }
+        catch [System.AggregateException]
+        {
+            # If the thing is not installed, we can't update it. In this case, we get an
+            # ArgumentException with the message "Value does not fall within the expected
+            # range." I cannot figure out why *that* is the error in the case of "app is
+            # not installed"... perhaps we could be doing something different/better, but
+            # I'm happy to just let this slide for now.
+            $problem = $_.Exception.InnerException # we'll just take the first one
+            Write-Host "Error updating app $app : $problem"
+            Write-Host "(this is expected if the app is not installed; you can probably ignore this)"
+        }
+        catch
+        {
+            Write-Host "Unexpected error updating app $app : $_"
+        }
+    }
+
+    Write-Host "Store updates completed"
+}
+catch
+{
+    Write-Error "Problem updating store apps: $_"
+    Write-Error "Exiting.."
+    exit 1
+}
+
+<#
+.SYNOPSIS
+    Ensure the winget package is available in our user session.
+    This contains (recursively) applying proper permissions to the (shared) package cache, linking package matrix, and making binaries available on PATH.
+#>
 if (!(Get-Command "winget" -ErrorAction SilentlyContinue)) {
-    Write-Output "winget is not on the PATH. Forcing synchronous package install..."
-    # Synchronously wait for the windows app store to install the package
+    Write-Output "winget is not on the PATH. Forcing synchronous package install."
+    # Synchronously wait for the windows app store to finish preparation of this package for the current user
     Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 
     if (!(Get-Command "winget" -ErrorAction SilentlyContinue)) {
-        Write-Host "Adding winget to the PATH failed. This requires a manual fix. Exiting"
+        Write-Error "Adding winget to the PATH failed. This requires a manual fix. Exiting.."
         exit 1
     }
 }
 
-# == Second Forcing update of all windows store apps ==
-
-Write-Output "Forcing scan + update of all windows store apps (including winget)"
-Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName "UpdateScanMethod" 
-
-# == Third ensure winget has proper package index sources ==
-
+<#
+.SYNOPSIS
+    Ensure winget has available and valid package index data
+#>
 winget.exe source reset --force
 winget.exe source update
 # Synchronously wait for the windows app store to process the sources update
